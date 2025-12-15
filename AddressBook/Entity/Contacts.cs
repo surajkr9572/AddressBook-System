1d0b9145cdcfea35b59947206c2313847a256785
surajkr9572 <surajsingh43006@gmail.com>
2025-12-16 00:33:33 +0530

    UC1 Completed

---
+++ AddressBook/Entity/Contacts.cs
@@ -0,0 +1,75 @@
+﻿using AddressBook.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AddressBook.Entity
+{
+    public class Contacts
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private string zipCode;
+        private string phoneNumber;
+        private string email;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName= value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName= value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city=value; }
+        }
+        public string State
+        {
+            get { return state; }
+            set { state=value; }
+        }
+        public string ZipCode
+        {
+            get { return zipCode; }
+            set { zipCode=value; }
+        }
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            private set { phoneNumber=value; }
+
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                string pattern = @"^[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,}$";
+                if (Regex.IsMatch(value, pattern))
+                {
+                    email = value;
+                }
+                else
+                {
+                    throw new EmailException("Invalid email format");
+                }
+            }
+        }
+    }
+}
